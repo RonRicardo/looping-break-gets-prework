86f905d55bb1059548c42dd0e9f773158c0e6105
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 00:03:28 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,8 @@
 
 def levitation_quiz
+  puts "What is the spell that enacts levitation?"
   question = gets
 	while question != "Wingardium Leviosa"
-    puts "What is the spell that enacts levitation?"
+  puts "What is the spell that enacts levitation?"
     break if question == "Wingardium Leviosa"
 end
